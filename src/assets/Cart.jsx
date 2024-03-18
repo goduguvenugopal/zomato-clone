f88d0f5528cebd838e3179373d008f5a8aab615e
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-03-18 16:58:02 +0530

    added order modal

--- src/assets/Cart.jsx
+++ src/assets/Cart.jsx
@@ -5,6 +5,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Orders = (props) => {
   const [cart, setCart] = useState(props.orders);
+  const [order, setOrder] = useState([]);
+  const [count, setCount] = useState(1);
 
   const notify = () => toast("Successfully Removed Item from Cart");
 
@@ -13,6 +15,21 @@ const Orders = (props) => {
     setCart(updatedCart);
     notify();
     console.log("After removal:", updatedCart);
+  };
+
+  const placeOrder = (order) => {
+    const ordered = cart.find((item) => item.name === order.name);
+    setOrder([ordered]);
+    setCount(0);
+  };
+
+  const increFunc = () => {
+    // Increment count by 1
+    setCount(count + 1);
+  };
+  const decreFunc = () => {
+    // Increment count by 1
+    setCount(count - 1);
   };
 
   return (
@@ -32,11 +49,20 @@ const Orders = (props) => {
       <div className="pb-5 pt-4 non-map-card">
         {cart && cart.length > 0 ? (
           cart.map((cartItem, index) => (
-            <div key={index} className="card1 "   >
+            <div key={index} className="card1 " style={{ height: "390px" }}>
               <img src={cartItem.url} className="card-img" alt="..." />
               <div className="card-b">
                 <h5 className="card-title">{cartItem.name}</h5>
-                <p className="card-text">{cartItem.title} </p>
+                <div className="pb-2 d-flex justify-content-between align-items-center">
+                  <h6 className="card-text title-txt">{cartItem.title}</h6>
+                  <div className=" d-flex justify-content-between align-items-center">
+                    <span className="material-symbols-outlined rupee-icon">
+                      currency_rupee
+                    </span>
+                    <h6 className="title-txt">{cartItem.cost} for one</h6>
+                  </div>
+                </div>
+
                 <div className="order-bt-card">
                   <button
                     onClick={() => removeFunc(cartItem)}
@@ -47,7 +73,12 @@ const Orders = (props) => {
                     </span>
                     Remove
                   </button>
-                  <button className="order-bt btn btn-primary">
+                  <button
+                    onClick={() => placeOrder(cartItem)}
+                    className="order-bt btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#staticBackdrop"
+                  >
                     Place Order
                   </button>
                 </div>
@@ -61,6 +92,82 @@ const Orders = (props) => {
           </div>
         )}
       </div>
+      <>
+        {/* Modal */}
+        <div
+          className="modal fade"
+          id="staticBackdrop"
+          data-bs-backdrop="static"
+          data-bs-keyboard="false"
+          tabIndex={-1}
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="staticBackdropLabel">
+                 Place Order
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="modal-body">
+                {order.map((item, index) => (
+                  <div className="card p-2" key={index}>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="d-flex justify-content-between align-items-center">
+                        <img src={item.url} className="order-img" alt="..." />
+                        <div className="meals-card">
+                          <h4 className="">{item.name}</h4>
+                          <h6 className="title-txt">{item.title}</h6>
+                          <div className="d-flex justify-content-start align-items-center">
+                            <span className="material-symbols-outlined rupee-icon">
+                              currency_rupee
+                            </span>
+                            <h6 className="title-txt">{item.cost}</h6>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className=" d-flex flex-column  align-items-center ">
+                        <h4 className="">Add Item </h4>
+                        <div className="counter-card">
+                          <span
+                            onClick={() => decreFunc(item)}
+                            class="material-symbols-outlined countbt"
+                          >
+                            remove
+                          </span>
+
+                          <h6 className="count">{count}</h6>
+
+                          <span
+                            onClick={() => increFunc(item)}
+                            class="material-symbols-outlined countbt"
+                          >
+                            add
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="modal-footer">
+                
+                <button type="button" className="btn btn-primary">
+                  Place Order
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
     </>
   );
 };
--- src/assets/data.js
+++ src/assets/data.js
@@ -39,32 +39,85 @@ export const states = [
 // foods Array
 
 export const foodList = [
-  { name: "Idly", title: "Andhra ,Indian", url: "/image/17.jpg",cost:"359" },
-  { name: "Masala Dosa", title: "South Indian", url: "/image/16.jpg",cost:"250" },
-  { name: "Meals", title: "South , Indian", url: "/image/15.jpg",cost:"209" },
-  { name: "Dosa", title: "Andhra,Indian Food", url: "/image/14.jpg",cost:"259" },
-  { name: "Rice dal", title: "Andhra ,Food", url: "/image/13.jpeg",cost:"399" },
-  { name: "Fried Rice", title: "Indian Food", url: "/image/12.jpeg",cost:"350" },
+  { name: "Idly", title: "Andhra ,Indian", url: "/image/17.jpg", cost: "359" },
+  {
+    name: "Masala Dosa",
+    title: "South Indian",
+    url: "/image/16.jpg",
+    cost: "250",
+  },
+  { name: "Meals", title: "South , Indian", url: "/image/15.jpg", cost: "209" },
+  {
+    name: "Dosa",
+    title: "Andhra,Indian Food",
+    url: "/image/14.jpg",
+    cost: "259",
+  },
+  {
+    name: "Rice dal",
+    title: "Andhra ,Food",
+    url: "/image/13.jpeg",
+    cost: "399",
+  },
+  {
+    name: "Fried Rice",
+    title: "Indian Food",
+    url: "/image/12.jpeg",
+    cost: "350",
+  },
   {
     name: "Vegetable Biryani",
     title: "South,Indian Food",
-    url: "/image/11.jpeg",cost:"119"
+    url: "/image/11.jpeg",
+    cost: "119",
   },
-  { name: "Biryani", title: "Hyderabad Biryani", url: "/image/10.jpeg",cost:"299" },
-  { name: "Fried Chicken", title: "Andhra, Food", url: "/image/9.jpeg",cost:"450" },
-  { name: "Fried Rice", title: "Andhra, Food", url: "/image/8.jpeg",cost:"150" },
-  { name: "Bread Puff", title: "Fast ,Food", url: "/image/7.jpeg" ,cost:"170"},
-  { name: "Starters", title: "Fast , Food Indian", url: "/image/6.jpeg" ,cost:"220"},
-  { name: "Veg Burger", title: "Fast,Food", url: "/image/5.jpeg",cost:"290" },
-  { name: "Burger", title: "Fast, Food", url: "/image/4.jpeg" ,cost:"189"},
-  { name: "Fried dish", title: "Andhra,Fast Food", url: "/image/3.jpeg" ,cost:"135"},
-  { name: "Fish Fry", title: "Andhra,Fish Food", url: "/image/2.jpeg" ,cost:"210"},
-  
+  {
+    name: "Biryani",
+    title: "Hyderabad Biryani",
+    url: "/image/10.jpeg",
+    cost: "299",
+  },
+  {
+    name: "Fried Chicken",
+    title: "Andhra, Food",
+    url: "/image/9.jpeg",
+    cost: "450",
+  },
+  {
+    name: "Fried Rice",
+    title: "Andhra, Food",
+    url: "/image/8.jpeg",
+    cost: "150",
+  },
+  {
+    name: "Bread Puff",
+    title: "Fast ,Food",
+    url: "/image/7.jpeg",
+    cost: "170",
+  },
+  {
+    name: "Starters",
+    title: "Fast , Food Indian",
+    url: "/image/6.jpeg",
+    cost: "220",
+  },
+  { name: "Veg Burger", title: "Fast,Food", url: "/image/5.jpeg", cost: "290" },
+  { name: "Burger", title: "Fast, Food", url: "/image/4.jpeg", cost: "189" },
+  {
+    name: "Fried dish",
+    title: "Andhra,Fast Food",
+    url: "/image/3.jpeg",
+    cost: "135",
+  },
+  {
+    name: "Fish Fry",
+    title: "Andhra,Fish Food",
+    url: "/image/2.jpeg",
+    cost: "210",
+  },
 ];
 
-
 export const scroll = [
-
   "Sort",
   "Nearest",
   "Rating 4.0+",
@@ -78,5 +131,12 @@ export const scroll = [
   "Dishes",
   "Restuarants",
   "Biryani",
-  "Frequent Orders"
-]
+  "Frequent Orders",
+  "Drinks (Bevarages)",
+  "Breads",
+  "Main Course",
+  "Soups",
+  "Fried Rice",
+
+  "Special Meals",
+];
