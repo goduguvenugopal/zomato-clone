c0b5b128c286ee1bb2a4fd8ac3ef7963eb218029
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-04-07 12:42:24 +0530

    add

--- server/routes/employeeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const employeeController = require('../controllers/employeeController');
-
-// POST route for adding an employee
-router.post('/add-emp', employeeController.createEmployee);
-
-module.exports = router;
